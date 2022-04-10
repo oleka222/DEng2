d66f47da15bcb07bdfd9c1faecca0e9abc94bc45
root <aleksanderandrzejewski222@gmail.com>
2022-04-10 09:53:38 +0000

    finished C1

--- C1/consumer2.py
+++ C1/consumer2.py
@@ -1,11 +1,16 @@
 import pulsar
 client = pulsar.Client('pulsar://localhost:6650')
 consumer = client.subscribe('DEtopic2', subscription_name = 'DE-sub2')
-msg = consumer.receive()
-try: 
-    print("Received message : '%s'" % msg.data()) 
-         
+producer3 = client.create_producer('DEtopic3')
+while(True):
+    msg = consumer.receive()
+    try:
+        print(msg.data().decode('utf-8').upper())
+        producer3.send((msg.data().decode('utf-8').upper()).encode('utf-8'))
+        
     # Acknowledge for receiving the message  
-    consumer.acknowledge(msg)
-except:
-    consumer.negative_acknowledge(msg)
+        consumer.acknowledge(msg)
+    except:
+        consumer.negative_acknowledge(msg)
+
+client.close()
---
+++ C1/consumer3.py
@@ -0,0 +1,17 @@
+import pulsar
+client = pulsar.Client('pulsar://localhost:6650') 
+ 
+# Subscribe to a topic and subscription  
+consumer = client.subscribe('DEtopic3', subscription_name='DE-sub3')
+sentence = ''
+while(True):
+    msg = consumer.receive()
+    try:
+        
+        sentence = sentence + msg.data().decode('utf-8') + ' '
+    
+        consumer.acknowledge(msg)
+    except:
+        consumer.negative_acknowledge(msg)
+    print(sentence)
+client.close()
